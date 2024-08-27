135347644015fbc7cecdfad1ab14fe5186b603eb
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-08-27 20:12:03 +1000

    Added comments to current code

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -1,3 +1,4 @@
+// Declare data to use in the logic
 let dataArray = [
     "Warthog",
     "Interceptor",
@@ -12,17 +13,24 @@ let dataArray = [
     "Toyota Corolla"
 ];
 
+// Find elements on the page to use in logic
 let carsContainerElement = document.getElementById("carsContainer");
+// Create elements to add to the page via the logic
 let carsContainerList = document.createElement("ul");
 
+// Do logic for each individual item in the array
 dataArray.forEach((car) => {
 	console.log(car);
-
+    
+    // Create a new HTML element to help us format the data's value
     let newCarEntry = document.createElement("li");
 
+    // Add the data to the new element 
     newCarEntry.innerText += car + "\n";
 
+    // Add the formatted element into the list container
     carsContainerList.appendChild(newCarEntry);
 });
 
+// Add the list container onto the actual HTML page
 carsContainerElement.appendChild(carsContainerList);
