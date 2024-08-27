29d00de9823cee50c56f408be24143c87e86ad7a
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-08-27 20:19:30 +1000

    Added remove button to each item

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -21,12 +21,22 @@ let carsContainerList = document.createElement("ul");
 // Do logic for each individual item in the array
 dataArray.forEach((car) => {
 	console.log(car);
-    
+
     // Create a new HTML element to help us format the data's value
     let newCarEntry = document.createElement("li");
 
     // Add the data to the new element 
     newCarEntry.innerText += car + "\n";
+
+    // Added button to each entry
+
+    // Click on the button to remove the entry from the list
+    let removeButton = document.createElement("button");
+    // removeButton.innerText = "Remove " + car;
+    removeButton = `Remove ${car}`;
+    // Add the removeButton to the car entry
+    newCarEntry.appendChild(removeButton);
+
 
     // Add the formatted element into the list container
     carsContainerList.appendChild(newCarEntry);
