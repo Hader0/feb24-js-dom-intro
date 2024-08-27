155381408446d462e132eb345a4d90db4c4af7f4
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-08-27 19:37:37 +1000

    HTML Skeleton and Data List initiated

---
+++ src/scripts/dataList.js
@@ -0,0 +1,17 @@
+let dataArray = [
+    "Warthog",
+    "Interceptor",
+    "Herbie",
+    "Lightning McQueen",
+    "Batmobile",
+    "Bat Tumbler",
+    "Cybertruck",
+    "Delorean",
+    "Elanore",
+    "Magic School Bus",
+    "Toyota Corolla"
+];
+
+dataArray.forEach(plants) => {
+	console.log(plants);
+});
